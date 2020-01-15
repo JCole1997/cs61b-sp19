8d5457fb2ac5a701cac0180dfbfed791f4632e06
JCole1997 <237142549@qq.com>
2020-01-15 17:08:15 +0800

    finished

--- lab6/UnionFind.java
+++ lab6/UnionFind.java
@@ -1,35 +1,47 @@
 public class UnionFind {
 
     // TODO - Add instance variables?
+    public int[] disjointSet;
 
     /* Creates a UnionFind data structure holding n vertices. Initially, all
        vertices are in disjoint sets. */
     public UnionFind(int n) {
         // TODO
+        disjointSet = new int[n];
+        for (int i = 0; i < n; i += 1) {
+            disjointSet[i] = i;
+        }
     }
 
     /* Throws an exception if v1 is not a valid index. */
     private void validate(int vertex) {
         // TODO
+        if (vertex < 0 || vertex >= disjointSet.length) {
+            throw new IllegalArgumentException("not a valid index");
+        }
     }
 
     /* Returns the size of the set v1 belongs to. */
     public int sizeOf(int v1) {
         // TODO
-        return -1;
+        validate(v1);
+        return -disjointSet[find(v1)];
     }
 
     /* Returns the parent of v1. If v1 is the root of a tree, returns the
        negative size of the tree for which v1 is the root. */
     public int parent(int v1) {
         // TODO
-        return -1;
+        validate(v1);
+        return disjointSet[v1];
     }
 
     /* Returns true if nodes v1 and v2 are connected. */
     public boolean connected(int v1, int v2) {
         // TODO
-        return false;
+        validate(v1);
+        validate(v2);
+        return find(v1) == find(v2);
     }
 
     /* Connects two elements v1 and v2 together. v1 and v2 can be any valid 
@@ -39,13 +51,34 @@ public class UnionFind {
        change the sets but may alter the internal structure of the data. */
     public void union(int v1, int v2) {
         // TODO
+        validate(v1);
+        validate(v2);
+        if (!connected(v1, v2)) {
+            if(sizeOf(v1) <= sizeOf(v2)) {
+                disjointSet[find(v2)] -= sizeOf(v1);
+                disjointSet[find(v1)] = find(v2);
+            }
+            else {
+                disjointSet[find(v1)] -= sizeOf(v2);
+                disjointSet[find(v2)] = find(v1);
+            }
+        }
     }
 
     /* Returns the root of the set V belongs to. Path-compression is employed
        allowing for fast search-time. */
     public int find(int vertex) {
         // TODO
-        return -1;
+        validate(vertex);
+        int root = vertex;
+        while(parent(root) >= 0) {
+            root = parent(root);
+        }
+        while(vertex != root) {
+            disjointSet[vertex] = root;
+            vertex = parent(vertex);
+        }
+        return root;
     }
 
 }
